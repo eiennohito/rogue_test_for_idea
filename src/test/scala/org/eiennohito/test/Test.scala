51ec935f2b63614364e190b5588c9869d3689ae2
Arseny Tolmachev <eiennohito@gmail.com>
2011-11-27 11:12:51 +0600

    fixed for error with long fields

--- src/test/scala/org/eiennohito/test/Test.scala
+++ src/test/scala/org/eiennohito/test/Test.scala
@@ -2,10 +2,10 @@ package org.eiennohito.test
 
 import net.liftweb.mongodb.record.{MongoMetaRecord, MongoRecord}
 import net.liftweb.mongodb.record.field.LongPk
-import net.liftweb.record.field.StringField
 import com.foursquare.rogue.Rogue._
 import net.liftweb.mongodb.{DefaultMongoIdentifier, MongoDB}
 import com.mongodb.Mongo
+import net.liftweb.record.field.{LongField, StringField}
 
 
 /**
@@ -17,6 +17,7 @@ class TestRecord private() extends MongoRecord[TestRecord] with LongPk[TestRecor
   def meta = TestRecord
 
   object field1 extends StringField(this, 50)
+  object longField extends LongField(this)
 }
 
 object TestRecord extends TestRecord with MongoMetaRecord[TestRecord]
@@ -28,7 +29,12 @@ class Test extends org.scalatest.FunSuite with org.scalatest.matchers.ShouldMatc
     val v = TestRecord.createRecord.field1("smt").save
 
     val rec = TestRecord where (_.field1 eqs "smt") get()
+
+    TestRecord where (_.id eqs 2L) and (_.longField eqs 3L)
+
     rec should not be None
     rec.get.id.is should equal (v.id.is)
+    
+    net.liftweb.sitemap.*
   }
 }
